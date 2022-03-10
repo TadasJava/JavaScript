e9b5d1b0dcd2cf3692faf9ca189564e2f26f6465
TadasJava <info@vindra.lt>
2022-03-10 22:03:21 +0200

    naujasjava

---
+++ index.js
@@ -0,0 +1,43 @@
+// Skaičiai
+//
+//var metai = 22
+
+//metai
+//22
+//let naujimetai = 21;
+
+//naujimetai + metai
+
+//let sum =0
+
+//sum ++
+//0
+//sum+=1
+//2
+///sum=sum+1
+//3
+//sum--
+//3
+//sum-=1
+//1
+//sum=sum-1
+//0
+//let metai=2022
+
+
+console.log ('Java Script')
+let skaicius=20;
+let kitasskaicius=7;
+let liekana =skaicius%kitasskaicius;
+console.log (skaicius%kitasskaicius);
+console.log (liekana);
+let vardas ='Tadas';
+let pavarde ='Zukelis';
+let gimimometai = '1974';
+let dabartiniaimetai= '2022';
+let atsakymas = `${vardas} ${pavarde} man yra ${dabartiniaimetai - gimimometai} `;
+
+console.log (atsakymas);
+
+let name1='mano vardas yra';
+console.log(name1.charAt(0));
